f505894324d68b3bcca79e917e449685fcf8494b
balajin96 <balajinatarajan18011996@gmail.com>
2026-02-12 10:50:31 +0530

    commiting all all js project in same folder to deploy in vercel

--- 1.passwordgenerator/index.js
+++ 1.passwordgenerator/index.js
@@ -1,31 +1,39 @@
 const btnEl = document.querySelector(".jsbtn");
 const inputEl = document.querySelector("input");
 const copyEl = document.querySelector(".copyPswd");
+/**
+ * CSS selector for range input
+ *  const rangeEl = document.querySelector("input[type='range']");
+ */
+const rangeEl = document.getElementById("getRange");
 
-console.log(inputEl);
+console.log(rangeEl.value);
 console.log(inputEl.__proto__);
 console.log(inputEl.__proto__.__proto__);
 console.log(inputEl.__proto__.__proto__.__proto__);
 console.log(inputEl.__proto__.__proto__.__proto__.__proto__);
 console.log(inputEl.__proto__.__proto__.__proto__.__proto__.__proto__);
 
+rangeEl.addEventListener("input", () => { getRangeValue() });
 
-btnEl.addEventListener("click", () => {
-    createPassword();
-});
+btnEl.addEventListener("click", () => { createPassword() });
 
-copyEl.addEventListener("click", () => {
-    copyPassword();
-});
+copyEl.addEventListener("click", () => { copyPassword() });
+
+function getRangeValue() {
+    console.log(rangeEl.value);
+    return parseInt(rangeEl.value)
+}
 
 function createPassword() {
-    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const passwordLength = 12;
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+    const passwordLength = getRangeValue();
+    console.log(passwordLength)
     let password = "";
 
     for (i = 0; i < passwordLength; i++) {
         const randomMath = Math.floor(Math.random() * chars.length)
-        console.log(randomMath)
+        // console.log(randomMath)
         password += chars.substring(randomMath, randomMath + 1)
     }
 
