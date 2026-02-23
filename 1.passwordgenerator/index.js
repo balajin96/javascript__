1d47dd0d8eb8b03de15c7adb96bf5372b5f680c3
balajin96 <balajinatarajan18011996@gmail.com>
2026-02-23 11:47:19 +0530

    range number

--- 1.passwordgenerator/index.js
+++ 1.passwordgenerator/index.js
@@ -6,13 +6,14 @@ const copyEl = document.querySelector(".copyPswd");
  *  const rangeEl = document.querySelector("input[type='range']");
  */
 const rangeEl = document.getElementById("getRange");
+const rangeValueEl = document.getElementById("rangeVal");
 
-console.log(rangeEl.value);
-console.log(inputEl.__proto__);
-console.log(inputEl.__proto__.__proto__);
-console.log(inputEl.__proto__.__proto__.__proto__);
-console.log(inputEl.__proto__.__proto__.__proto__.__proto__);
-console.log(inputEl.__proto__.__proto__.__proto__.__proto__.__proto__);
+// console.log(rangeEl.value);
+// console.log(inputEl.__proto__);
+// console.log(inputEl.__proto__.__proto__);
+// console.log(inputEl.__proto__.__proto__.__proto__);
+// console.log(inputEl.__proto__.__proto__.__proto__.__proto__);
+// console.log(inputEl.__proto__.__proto__.__proto__.__proto__.__proto__);
 
 rangeEl.addEventListener("input", () => { getRangeValue() });
 
@@ -22,6 +23,7 @@ copyEl.addEventListener("click", () => { copyPassword() });
 
 function getRangeValue() {
     console.log(rangeEl.value);
+    rangeValueEl.textContent = rangeEl.value;
     return parseInt(rangeEl.value)
 }
 
@@ -30,13 +32,11 @@ function createPassword() {
     const passwordLength = getRangeValue();
     console.log(passwordLength)
     let password = "";
-
     for (i = 0; i < passwordLength; i++) {
         const randomMath = Math.floor(Math.random() * chars.length)
         // console.log(randomMath)
         password += chars.substring(randomMath, randomMath + 1)
     }
-
     inputEl.value = password;
     console.log(password);
 
@@ -45,5 +45,4 @@ function createPassword() {
 function copyPassword() {
     inputEl.select()
     navigator.clipboard.writeText(inputEl.value)
-    alert("Password copied to clipboard!");
 }
